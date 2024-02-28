4da9c99a72acbe1f28882caae23b37557c9746b1
Jiyun <grosjeanjiyun@icloud.com>
2024-02-28 17:03:23 +0100

    env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase, { defaultConfig }) => {
@@ -37,20 +36,3 @@ module.exports = (phase, { defaultConfig }) => {
     },
   };
 };
-
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: ["res.cloudinary.com"],
-//   },
-//   env: {
-//     mongodb_username: "jiyun",
-//     mongodb_password: "1VBQWdxngYJTxg1P",
-//     mongodb_clustername: "cluster0",
-//     next_public_api_url: `http://localhost:3000`,
-//   },
-// };
-
-// export default nextConfig;
-
-// import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
