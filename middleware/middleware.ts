5b54ab3342880f3657b3fce973a984c4d4284246
Jiyun <grosjeanjiyun@icloud.com>
2024-03-03 20:56:45 +0100

    login google, middleware

---
+++ middleware/middleware.ts
@@ -0,0 +1,4 @@
+// protected route with next auth
+export { default } from "next-auth/middleware";
+
+// export const config = { matcher: ["/", "/about"] };
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,18 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const options = {
+export const options: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID ?? "",
-      clientSecret: process.env.FACEBOOK_SECRET ?? "",
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
       authorization: {
         params: {
-          scope: "email,public_profile", // Specify scopes here, separated by commas
+          scope: "email,public_profile",
         },
       },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 };
