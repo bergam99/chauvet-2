ab3152f8542f78ddaf98b8c34cb1520a78239644
Jiyun <grosjeanjiyun@icloud.com>
2024-03-06 18:06:52 +0100

    product clean code

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,25 +1,27 @@
-// /api/products
-import { IProduct } from "./../../types/product";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { connectDB } from "../../utils/connectDB";
-interface ApiResponse {
-  message: string;
-  products?: IProduct[];
-}
+// i don't need this api
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ApiResponse>
-): Promise<void> {
-  try {
-    const db = await connectDB();
-    const products = await db
-      .collection<IProduct>("products")
-      .find({})
-      .toArray();
-    res.status(200).json({ message: "api/products:ok ", products });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Failed to fetch products" });
-  }
-}
+// // /api/products
+// import { IProduct } from "./../../types/product";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { connectDB } from "../../utils/connectDB";
+// interface ApiResponse {
+//   message: string;
+//   products?: IProduct[];
+// }
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<ApiResponse>
+// ): Promise<void> {
+//   try {
+//     const db = await connectDB();
+//     const products = await db
+//       .collection<IProduct>("products")
+//       .find({})
+//       .toArray();
+//     res.status(200).json({ message: "api/products:ok ", products });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Failed to fetch products" });
+//   }
+// }
