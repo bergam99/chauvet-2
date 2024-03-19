18ee13b590559a4536cf8f33220a2395fe88b410
Jiyun <grosjeanjiyun@icloud.com>
2024-03-19 17:25:12 +0100

    import vercel error

--- next.config.mjs
+++ next.config.mjs
@@ -10,17 +10,14 @@
 //     ],
 //   },
 // };
+import path from "path";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
-};
-
-const path = require("path");
-
-module.exports = {
-  webpack(config) {
+  webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "./src/");
     return config;
   },
