49b5e3a2b8ef6792d1f163aacf139fdcb1499aef
Jiyun <grosjeanjiyun@icloud.com>
2024-03-22 18:49:00 +0100

    style

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import classes from "./404.module.css";
+
 const Error = () => {
-  return <div>custum 404 page not found!</div>;
+  return (
+    <>
+      <p className={classes.txt}>Mauvais chamin . . .</p>
+      <Link href="/" className={`${classes.btn} DefaultButton`}>
+        <button>Retour à l&apos;accueil</button>
+      </Link>
+    </>
+  );
 };
 
 export default Error;
