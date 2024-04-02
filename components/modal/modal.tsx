9051a92e1ea832debc09690021550a25bfc0ff62
Jiyun <grosjeanjiyun@icloud.com>
2024-04-02 14:41:22 +0200

    cart css

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -8,8 +8,6 @@ import {
 import { createPortal } from "react-dom";
 import classes from "./modal.module.css";
 import Link from "next/link";
-import Image from "next/image";
-import x from "@/public/icon/close.png";
 import Close from "../buttons/close/close";
 interface ModalHandles {
   open: () => void;
@@ -47,7 +45,7 @@ const Modal = forwardRef<ModalHandles>((props, ref) => {
           <p className={classes.txt}>1 {} a été ajouté dans votre panier.</p>
 
           <div className={classes.btnContainer}>
-            <form method="dialog" className="DefaultButton">
+            <form method="dialog" className={`${classes.btn1} DefaultButton`}>
               <button>Continuer mes achats</button>
             </form>
             <Link href="/cart" className="DefaultButtonDark">
