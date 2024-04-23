e9f1f631d18d8af0ed105bbe03a83a2632cc96c6
Jiyun <grosjeanjiyun@icloud.com>
2024-04-23 22:02:12 +0200

    clear cart once order success

--- pages/payment-success/index.tsx
+++ pages/payment-success/index.tsx
@@ -1,6 +1,15 @@
+import { useCartStore } from "@/stores/cart";
 import Link from "next/link";
+import { useEffect } from "react";
 
 const PaymentSuccess = () => {
+  const { clearCart } = useCartStore();
+
+  useEffect(() => {
+    clearCart();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <div>PaymentSuccess</div>
