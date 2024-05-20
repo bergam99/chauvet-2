91d8c4d9dcaac232c16420c860c05818903f97d1
Jiyun <grosjeanjiyun@icloud.com>
2024-05-20 17:38:05 +0200

    fix retour a form page et vider(reset) localId

--- components/address/allAddresses/allAddresses.tsx
+++ components/address/allAddresses/allAddresses.tsx
@@ -1,14 +1,19 @@
 import classes from "./allAddresses.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useAddressStore } from "@/stores/address";
 import MapAllAddresses from "../mapAllAddresses/mapAllAddresses";
 import OpenModalBtn from "../../openModalBtn/openModalBtn";
 
 const AllAddresses = () => {
-  const { shippingAddress } = useAddressStore();
+  const { shippingAddress, resetShippingAddress } = useAddressStore();
   const [validationError, setValidationError] = useState("");
   const router = useRouter();
+
+  useEffect(() => {
+    resetShippingAddress();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleValidationAndClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
